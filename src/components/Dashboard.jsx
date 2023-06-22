22df942117a54a54f4e568ae2922c7a65c2be56c
Domains18 <gibsonsgibson88@gmail.com>
2023-06-22 22:54:41 +0300

    commmiting styles and alpha
    Signed-off-by: Domains18 <gibsonsgibson88@gmail.com>

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
-
+import { AiFillDashboard } from 'react-icons/ai'
+import { CgProfile } from 'react-icons/cg'
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <section className='container'>
+      <div className="left">
+        <div className="logo">
+          <img src="https://via.placeholder.com/150" alt="logo" />
+        </div>
+        <div className="separator">
+          <p><AiFillDashboard className='dash-icon' />Dashboard</p>
+        </div>
+        <section className="options">
+          <div className="option">
+            <p><CgProfile className='dash-icon' />Profile</p>
+          </div>
+        </section>
+      </div >
+      {/* center */}
+      <div className="center"></div>
+      {/* right */}
+      <div className="right" > right</div >
+    </section >
   )
 }
 
