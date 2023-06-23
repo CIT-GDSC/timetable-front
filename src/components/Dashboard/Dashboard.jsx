4529ac48b1ec7a0c69c98431bae9a117d89ae418
Domains18 <gibsonsgibson88@gmail.com>
2023-06-23 03:14:32 +0300

    commmiting styles onalpha
    Signed-off-by: Domains18 <gibsonsgibson88@gmail.com>

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+
 import { AiFillDashboard } from 'react-icons/ai';
 import { CgProfile } from 'react-icons/cg';
 import { IoMdPeople } from 'react-icons/io'
 import { PiChalkboardTeacherBold } from 'react-icons/pi'
+import { GiArchiveRegister } from 'react-icons/gi'
+import { MdOutlineWhatsapp } from 'react-icons/md'
+import { MdAdUnits } from 'react-icons/md'
+import { BsTable } from 'react-icons/bs'
+import { TbMathXy } from 'react-icons/tb'
+
+
 const Dashboard = () => {
   return (
     <section className='container'>
@@ -23,15 +31,57 @@ const Dashboard = () => {
             <p>Trainers</p>
           </div>
           <div className="option">
-            <p><IoMdPeople className='dash-icon' />ClassMates</p>
+            <IoMdPeople className='dash-icon' />
+            <p>ClassMates</p>
           </div>
           <div className="option">
-            <p><IoMdPeople className='dash-icon' />Profile</p>
+            <BsTable className='dash-icon' />
+            <p>TimeTable</p>
+          </div>
+          <div className="option">
+            <GiArchiveRegister className='dash-icon' />
+            <p>Attendances</p>
+          </div>
+          <div className="option">
+            <MdAdUnits className='dash-icon' />
+            <p>My units</p>
+          </div>
+          <div className="option">
+            <MdOutlineWhatsapp className='dash-icon' />
+            <p>Class group</p>
           </div>
         </section>
       </div >
       {/* center */}
-      <div className="center"></div>
+      <div className="center">
+        <div className="top">
+          <div className="username">
+            <h3>Hello, User</h3>
+            <div className="date-time">
+              <div className="top-left">
+                <h1 className='title'>Timetable</h1>
+                <p><span className="day">Monday</span><span className="month">July</span><span className="year">2021</span></p>
+              </div>
+              <div className="top-right">
+                <p className="date">
+                  12/12/2021 <BsTable />
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="bottom">
+          <article className="timetable">
+            <div className="time">8:00 AM</div>
+            <div className="unit">
+              <div className="unit-icon dash-icon"><TbMathXy/></div>
+              <div className="unit-name">Information</div>
+              <div className="unit- lecturer"></div>
+            </div>
+          </article>
+        </div>
+      </div>
       {/* right */}
       <div className="right" > right</div >
     </section >
