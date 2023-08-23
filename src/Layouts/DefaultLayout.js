09655be4e33a58c3797868e4297546f413903cfc
Gibsons Gibson <gibsonsgibson88@gmail.com>
2023-08-23 08:48:48 +0300

    fixed deployment issues

--- src/Layouts/DefaultLayout.js
+++ src/Layouts/DefaultLayout.js
@@ -1,13 +1,11 @@
 //eslint-disable-next-line
 import React from 'react'
 import { Navigate, Outlet } from 'react-router-dom'
-// import { useStateContext } from '../Context'
 
 
 
 
 const DefaultLayout = () => {
-  // const { token } = useStateContext();
   const token = localStorage.getItem('ACCESS_TOKEN');
   console.log(token)
   //temporary configrations for d
